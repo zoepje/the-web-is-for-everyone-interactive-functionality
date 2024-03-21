3394ad391922c5bb73a42495aeee3b938ee9f9e7
zoepje <zoe.penner@hva.nl>
2024-03-21 15:30:36 +0100

    copy link

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,4 @@
+//#region Menu
 const menuBtn = document.querySelector('.menu-button'),
       // closeBtn = document.querySelector('.close-button'),
       nav = document.querySelector('nav'),
@@ -5,8 +6,21 @@ const menuBtn = document.querySelector('.menu-button'),
 
 // Functie uitklappen menu
 function toggleMenu(){
-  header.classList.toggle("nav-open");
+  header.classList.toggle('nav-open');
 }
 
 // als je klikt op dit element dan voer deze funtie uit.
-menuBtn.addEventListener("click", toggleMenu);
+menuBtn.addEventListener('click', toggleMenu);
+//#endregion Menu
+
+//#region Share
+const link = encodeURI(window.location.href);
+const shareBtn = document.querySelector('.deel-button');
+    
+function shareLink(){
+  navigator.clipboard.writeText(link);
+  shareBtn.classList.toggle('done');
+}
+
+shareBtn.addEventListener('click', shareLink)
+//#endregion Share
